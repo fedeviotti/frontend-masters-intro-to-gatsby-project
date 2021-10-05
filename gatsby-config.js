d9a512e2763cb8a5b87f9320f28311874440d59d
fedeviotti <fedeviotti@gmail.com>
2021-10-05 23:09:22 +0200

    Feat: Added Seo component

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.mydomain.tld',
+    title: 'Frontend Master Intro to Gatsby',
+    description: 'Frontend Master Intro to Gatsby Intro course projects',
+    image: 'https://res.cloudinary.com/fedeviotti/image/upload/v1633452322/sample.jpg',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet'
+  ]
+}
