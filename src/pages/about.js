24983d73d83944e1b1f12d5d3e886588ae6316a0
fedeviotti <fedeviotti@gmail.com>
2021-10-11 23:51:32 +0200

    Feat: Added images optimization

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,30 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
     <Layout
       title="About this site"
       description="More information about this site."
     >
+      <GatsbyImage
+        alt={
+          'a cocktail set inside an elaborate floral arrangement with dry ice mist curling out and around it'
+        }
+        image={getImage(data.file)}
+      />
       <h1>About This Site</h1>
       <Link to="/">Home</Link>
     </Layout>
